a13f2709fcabd3d167624a0de196f62aaabc0d53
chaewoon <bw06050@gmail.com>
2025-10-18 15:18:57 +0900

    :contstruction:sparkles:add GetCommentsAction

--- src/com/oopsw/action/employee/GetCommentsAction.java
+++ src/com/oopsw/action/employee/GetCommentsAction.java
@@ -2,32 +2,37 @@ package com.oopsw.action.employee;
 
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 import com.oopsw.action.Action;
 import com.oopsw.model.DAO.EmployeeDAO;
 import com.oopsw.model.VO.CommentVO;
 
-public class GetCommentsAction implements Action{
+public class GetCommentsAction implements Action {
 
 	@Override
 	public String execute(HttpServletRequest request) throws ServletException, IOException {
-		
-		HttpSession session = request.getSession();
-		int documentNum = (int) session.getAttribute("documentNumber");
-		
-		EmployeeDAO dao = new EmployeeDAO();
-		List<CommentVO> commentList = dao.getComments(documentNum);
-		
-		request.setAttribute("commentsResult", commentList);
-		
-		return "webpage/commentResult.jsp";
-		
-		  
-	}
 
+		int documentNo = Integer.parseInt(request.getParameter("documentNo"));        
+		EmployeeDAO dao = new EmployeeDAO();
+		List<CommentVO> commentList = dao.getComments(documentNo);
+
+
+		Gson gson = new Gson();
+		String json = gson.toJson(commentList);
+
+
+		HttpServletResponse response = (HttpServletResponse) request.getAttribute("javax.servlet.response");
+		if (response == null) {
+			throw new ServletException("HttpServletResponse를 찾을 수 없습니다.");
+		}
+		response.setContentType("application/json;charset=UTF-8");
+		response.getWriter().write(json);
+		response.getWriter().flush();
+
+		return null;
+	}
 }
